ba0fc4cff1369344db63268e8f518c79c2225632
sachin798815 <sachin798815@gmail.com>
2023-08-11 14:37:54 +0530

    Expense Item added

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Items</h2>
+      <div>
+        <div>Food Rs 10</div>
+        <div>petrol Rs 100</div>
+        <div>movies Rs 200</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
