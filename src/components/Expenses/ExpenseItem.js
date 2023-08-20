447842ed6c82e37ce706cde2f6de02a4de121903
sachin798815 <sachin798815@gmail.com>
2023-08-20 23:28:10 +0530

    event added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,13 +2,29 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 
-const ExpenseItem=(props)=> {
+const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("clicked");
+  };
+
+  const deleter = (e)=>{
+    e.target.parentNode.style.display = 'none';
+  }
+
+
   return (
     <div className="expense-item">
-      <ExpenseDate date={props.date}/>
-      <ExpenseDetails title={props.title} amount={props.amount} location={props.location}/>
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        title={props.title}
+        amount={props.amount}
+        location={props.location}
+      />
+      <button onClick={clickHandler}>Change Title</button>
+      <button
+      onClick={deleter}>Delete</button>
     </div>
   );
-}
+};
 
 export default ExpenseItem;
