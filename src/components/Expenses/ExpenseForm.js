d9052d963af3d6db787dd9d3df6ae800f74dd41c
sachin798815 <sachin798815@gmail.com>
2023-08-25 18:19:13 +0530

    States added

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,22 +1,36 @@
-const ExpenseForm = ()=>{
- 
+import { useState } from "react";
 
-    const displayer =(e)=>{
-        e.preventDefault();
-        console.log(e.target.value);
-    } 
+const ExpenseForm = ()=>{ 
+
+    const[enteredTitle,setTitle]=useState('');
+    const[enteredExpense ,setExpense] = useState ('');
+    const[enteredDate,setDate]=useState('');
+    const[enteredLocation,setLocation]=useState('');
+
+    const titleHandler=(e)=>{
+        setTitle(e.target.value)
+    }
+    const expenseHandler=(e)=>{
+        setExpense(e.target.value)
+    }
+    const locationHandler=(e)=>{
+        setLocation(e.target.value)
+    }
+    const dateHandler=(e)=>{
+        setDate(e.target.value)
+    }
 
     return(
         <div>
             <form action="">
                 <label htmlFor="dateInput">Expense date</label>
-                <input type="date" id="dateInput" name="dateinput" onChange={displayer}/>
+                <input type="date" id="dateInput" name="dateinput" onChange={dateHandler}/>
                 <label htmlFor="titleInput">Expense title</label>
-                <input type="text" id="titleInput" name="titleInput" onChange={displayer}/>
+                <input type="text" id="titleInput" name="titleInput" onChange={titleHandler}/>
                 <label htmlFor="expenseInput">Expense amount</label>
-                <input type="number" id="expenseInput" name="expenseInput" onChange={displayer}/>
+                <input type="number" id="expenseInput" name="expenseInput" onChange={expenseHandler}/>
                 <label htmlFor="locationInput">Expense location</label>
-                <input type="text" id="locationInput" name="locationInput" onChange={displayer}/>
+                <input type="text" id="locationInput" name="locationInput" onChange={locationHandler}/>
                 <input type="submit" onClick={()=>{}} value="add"/>
             </form>
         </div>
